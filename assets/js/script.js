606bffaeaa72a6997efc49c46477a19c9af43c19
ocean <osciris.veliz@gmail.com>
2022-07-07 18:47:11 -0500

    add cocktail api fetch

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,10 +6,26 @@
 // Romance
 // Sci-fi
 
+// Vodka, Tequila, Whiskey, Brandy, Gin, Amaretto, Champagne
+// COCKTAIL BY NAME: www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita
+// INGREDIANT ID: www.thecocktaildb.com/api/json/v1/1/lookup.php?iid=552
+// RANDOM COCKTAIL: www.thecocktaildb.com/api/json/v1/1/random.php
+// SEARCH BY INGREDIANT: www.thecocktaildb.com/api/json/v1/1/filter.php?i=Gin
+// CATEGORY, ORDINARY DRINK: www.thecocktaildb.com/api/json/v1/1/filter.php?c=Ordinary_Drink
 
-// Vodka
-// Tequila
-// Whiskey
-// Brandy
-// Gin
-// Amaretto
+// once they choose a movie then the cocktail functions will be ran
+function getDrink(drink) {
+  var drinkByName = "https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=" + drink;
+  console.log(drinkByName);
+  fetch(drinkByName)
+    .then(function(response){
+      if(response.ok) {
+        response.json()
+        .then(function(drinkData){
+          console.log(drinkData, "drink data fetched");
+        }) 
+      }// end if statment
+  });
+}
+
+getDrink("tequila");
