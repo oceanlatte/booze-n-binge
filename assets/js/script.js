c3b10913138ef47bd496d297a4c4e589e698bbb8
ocean <osciris.veliz@gmail.com>
2022-07-07 19:25:00 -0500

    add drink randomizer and pass to drink info function

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,13 +8,14 @@
 
 // Vodka, Tequila, Whiskey, Brandy, Gin, Amaretto, Champagne
 // COCKTAIL BY NAME: www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita
+// SEARCH BY INGREDIANT: www.thecocktaildb.com/api/json/v1/1/filter.php?i=Gin
+
 // INGREDIANT ID: www.thecocktaildb.com/api/json/v1/1/lookup.php?iid=552
 // RANDOM COCKTAIL: www.thecocktaildb.com/api/json/v1/1/random.php
-// SEARCH BY INGREDIANT: www.thecocktaildb.com/api/json/v1/1/filter.php?i=Gin
 // CATEGORY, ORDINARY DRINK: www.thecocktaildb.com/api/json/v1/1/filter.php?c=Ordinary_Drink
 
 // once they choose a movie then the cocktail functions will be ran
-function getDrink(drink) {
+function drinkChooser(drink) {
   var drinkByName = "https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=" + drink;
   console.log(drinkByName);
   fetch(drinkByName)
@@ -23,17 +24,35 @@ function getDrink(drink) {
         response.json()
         .then(function(drinkData){
           console.log(drinkData, "drink data fetched");
-          // var randomizer = Math.floor(Math.random() * drinkData.drinks.length);
-          // console.log(randomizer);
-         
-          // loop through all the drinks that come up by alcohol type
-          for (var i = 0; i < drinkData.drinks.length; i++){
-            var drinkName = drinkData.drinks[i].strDrink;
-            console.log(drinkName);
-          }//end for loop
+
+          // use randomized instead of for loop and where the [i] is for only one random drink
+          var randomizer = Math.floor(Math.random() * drinkData.drinks.length);
+
+          // get drink name and drink ID #, drink name will need to be displayed
+          var drinkName = drinkData.drinks[randomizer].strDrink;
+          var drinkId = drinkData.drinks[randomizer].idDrink;
+          console.log(drinkName, drinkId);
+
+          // drinkId to pass through drink information function
+          drinkInfo(drinkId);
         }) 
       }// end if statment
   });
 }
 
-getDrink("tequila");
+// use drinkId from drinkChooser function to get that specific drink's information
+function drinkInfo(drinkId) {
+  var drinkById = "https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=" + drinkId;
+
+  fetch(drinkById)
+    .then(function(response){
+      if(response.ok) {
+        response.json()
+        .then(function(data){
+          console.log(data);
+        })
+      }// end if statment
+    })
+};
+
+drinkChooser("tequila");
