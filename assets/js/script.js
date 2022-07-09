94d7ffc56efdde0342fd41547aa5cd3937a2e6db
Sczygelski <avamariescz@gmail.com>
2022-07-08 19:26:48 -0500

    ingredients list setup

--- assets/js/script.js
+++ assets/js/script.js
@@ -55,12 +55,26 @@ function drinkInfo(drinkId) {
           console.log(drinkImage);
 
           // need to loop through ingrediants
-          if (data.drinks[0].strIngredient15 === null) {
-            data.drinks[0].strIngredient15 = "";
-          } 
 
-          var ingrediants = data.drinks[0].strIngredient1 + data.drinks[0].strIngredient2 + ", " + data.drinks[0].strIngredient15;
-          console.log("first ingrediant listed is: " + ingrediants);
+          // if (data.drinks[0].strIngredient15 === null) {
+          //   data.drinks[0].strIngredient15 = " "}
+          // if (data.drinks[0].strIngredient15 === null) {
+          //   data.drinks[0].strIngredient15 = " "}
+          for (let i = 1; i <= 15; i++) {
+            ingredient = "strIngredient" + [i];
+            console.log(ingredient);
+            if (data.drinks[0].ingredient === null) {
+              console.log((data.drinks[0].ingredient));
+              data.drinks[0].ingredient = " "}
+          }
+
+
+          ingredients = (`${data.drinks[0].strIngredient1} \ ${data.drinks[0].strIngredient2} \ ${data.drinks[0].strIngredient3} \ ${data.drinks[0].strIngredient4} \ ${data.drinks[0].strIngredient5} \ ${data.drinks[0].strIngredient6} \ ${data.drinks[0].strIngredient7} \ ${data.drinks[0].strIngredient8} \ ${data.drinks[0].strIngredient9} \ ${data.drinks[0].strIngredient10} \ ${data.drinks[0].strIngredient11} \ ${data.drinks[0].strIngredient12} \ ${data.drinks[0].strIngredient13} \ ${data.drinks[0].strIngredient14} \ ${data.drinks[0].strIngredient15}`)
+
+
+          // var ingrediants = data.drinks[0].strIngredient1 + data.drinks[0].strIngredient2 + ", " + data.drinks[0].strIngredient15;
+
+          console.log("Ingredients listed are: " + ingredients);
 
 
           var instructions = "Instructions: " + data.drinks[0].strInstructions;
