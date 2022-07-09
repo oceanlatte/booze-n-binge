77cc36b0ca12b4e228ce38ccd915f8c07a418642
Gestan <104741659+Gestan24@users.noreply.github.com>
2022-07-09 17:45:37 -0500

    added button functionality, able to see info in console

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,4 @@
+var movieTitle = document.querySelector(".input").textContent;
 // Comedy
 // Animation
 // Drama
@@ -148,3 +149,20 @@ function drinkDisplayer(drink, image, instructions) { // may need to add ingredi
 
 }
 drinkChooser("gin");
+
+$(".button").click(function (event) {
+
+  event.preventDefault();
+
+  movieTitle = $(this).siblings(".input").val().trim();
+  console.log(movieTitle);
+
+  if (movieTitle) {
+
+    chooseMovie(movieTitle);
+
+    movieTitle = "";
+
+  }
+  
+})
